9831b476746c1e67744ac58b4e05dbf8dff1e0c1
AJR0703 <amaan.jahangir2003@gmail.com>
2026-06-21 19:21:07 +0100

    Handles login errors and loading state

--- react-test-app/src/features/auth/LoginPage.jsx
+++ react-test-app/src/features/auth/LoginPage.jsx
@@ -1,6 +1,7 @@
 import {useAuth} from "../../hooks/useAuth.js";
 import {Link, useNavigate} from "react-router-dom";
 import {AuthForm} from "./AuthForm.jsx";
+import {useState} from "react";
 
 /**
  * Handles login functionality.
@@ -11,16 +12,26 @@ import {AuthForm} from "./AuthForm.jsx";
 export function LoginPage() {
     const { login } = useAuth();
     const navigate = useNavigate();
+    const [error, setError] = useState(null);
+    const [submitting, setSubmitting] = useState(false);
 
     const handleLogin = async (credentials) => {
-        await login(credentials);
-        navigate("/dashboard");
+        setError(null);
+        setSubmitting(true);
+        try {
+            await login(credentials);
+            navigate("/dashboard");
+        } catch (err) {
+            setError("Invalid Login. Please check email and password");
+        } finally {
+            setSubmitting(false);
+        }
     };
 
     return (
         <div className="bg-dark text-white">
             <h1>Log in</h1>
-            <AuthForm submitLabel="Log in" onSubmit={handleLogin} />
+            <AuthForm submitLabel="Log in" onSubmit={handleLogin} error={error} submitting={submitting} />
             <p>
                 No Account? <Link to="/signup">Sign up</Link>
             </p>
