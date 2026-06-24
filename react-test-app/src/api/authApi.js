ce4e13706413f724f83ec6345c350ba20c054032
AJR0703 <amaan.jahangir2003@gmail.com>
2026-06-24 13:49:47 +0100

    Adds new workoutApi files for defining all api functions related to workouts.

--- react-test-app/src/api/authApi.js
+++ react-test-app/src/api/authApi.js
@@ -1,12 +1,15 @@
 import { apiFetch } from "./client.js";
 
 export function getProfile() {
-    return apiFetch("/profile", { method: "GET" });
+    return apiFetch("/profile",
+        {
+        method: "GET"
+    });
 }
 
 export function saveProfile({ name, email }) {
     return apiFetch("/profile", {
         method: "PUT",
         body: JSON.stringify({ name, email }),
-    })
+    });
 }
